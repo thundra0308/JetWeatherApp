9f14b7aa17007205869a6169fcfd06c3cca7ae69
thundra0308 <suryanshsingh03082001@gmail.com>
2024-09-16 23:43:11 +0530

    Commit_4

--- app/src/main/java/com/example/jetweatherapp/components/HorizontalForecastListOf24Hours.kt
+++ app/src/main/java/com/example/jetweatherapp/components/HorizontalForecastListOf24Hours.kt
@@ -5,16 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.jetweatherapp.model.WeatherData
 import com.example.jetweatherapp.screens.getLocalTimeFromUnixTimestamp
@@ -28,9 +23,7 @@ fun HorizontalForecastListOf24Hours(forecastData: WeatherData) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val scrollState = rememberLazyListState()
         LazyRow(
-            modifier = Modifier.fadingEdgeEffectHorizontal(scrollState),
             contentPadding = PaddingValues(horizontal = 15.dp, vertical = 0.dp),
             horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
@@ -42,31 +35,5 @@ fun HorizontalForecastListOf24Hours(forecastData: WeatherData) {
                 )
             }
         }
-    }
-}
-
-fun Modifier.fadingEdgeEffectHorizontal(scrollState: LazyListState): Modifier = this.drawBehind {
-    // Define the width of the fade at the start and end
-    val fadeWidth = 50.dp.toPx()
-
-    // Start (left) gradient fade
-    drawRect(
-        brush = Brush.horizontalGradient(
-            colors = listOf(Color.Transparent, Color.Black),
-            startX = 0f,
-            endX = fadeWidth
-        )
-    )
-
-    // End (right) gradient fade
-    val lastVisibleItemIndex = scrollState.layoutInfo.visibleItemsInfo.lastOrNull()?.index
-    if (lastVisibleItemIndex != null && lastVisibleItemIndex < scrollState.layoutInfo.totalItemsCount - 1) {
-        drawRect(
-            brush = Brush.horizontalGradient(
-                colors = listOf(Color.Black, Color.Transparent),
-                startX = size.width - fadeWidth,
-                endX = size.width
-            )
-        )
     }
 }
